02193bf8d8f5b48015f2886ac2254d70bc6c721f
OriginalCube <lancemiraflorcube@gmail.com>
2025-06-10 07:57:15 +0800

    feat: added new enhancements
    - Introduced matugen for color configuration
    - Created a theme switch script

---
+++ theme.sh
@@ -0,0 +1,12 @@
+THEME=2
+
+# Parse arguments
+if [[ $1 =~ ^--[0-9]+$ ]]; then
+    THEME=${1:2} # Extract the number after '--'
+else
+    echo "Usage: $0 --<theme_number>"
+    exit 1
+fi
+
+matugen image "$HOME/.local/personal-dotfiles/images/wallpapers/$THEME.jpg"
+swww img "$HOME/.local/personal-dotfiles/images/wallpapers/$THEME.jpg" --transition-type center
